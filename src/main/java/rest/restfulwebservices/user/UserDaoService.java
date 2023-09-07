25a85e65043914eb4813205001b542c86a224f5b
mason-9506 <mason.9506@kakaocorp.com>
2023-09-07 21:40:42 +0900

    UserDaoService 구현

---
+++ src/main/java/rest/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,37 @@
+package rest.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    // JPA|Hibernate >  database
+    // UserDaoService > Static List
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 0;
+
+    static {
+        users.add(new User(++usersCount, "A", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "B", LocalDate.now().minusYears(26)));
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+}
