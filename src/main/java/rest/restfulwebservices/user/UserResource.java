23d37967a25c8fe71058d25abf254f3db017b77d
mason-9506 <mason.9506@kakaocorp.com>
2023-09-07 21:41:59 +0900

    custom 예외 처리 구현

---
+++ src/main/java/rest/restfulwebservices/user/UserResource.java
@@ -0,0 +1,46 @@
+package rest.restfulwebservices.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null)
+            throw new UserNotFoundException("id:" + id);
+
+        return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        // post하고 response location부분에 저장한 user 보여주기
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
